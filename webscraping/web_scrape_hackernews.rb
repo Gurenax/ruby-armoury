f41aaf77e2c3ca22a777033b4c6e060fb6639e32
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-10 00:22:37 +1100

    Add webscraper for hackernews

---
+++ webscraping/web_scrape_hackernews.rb
@@ -0,0 +1,30 @@
+require 'httparty'
+require 'nokogiri'
+require 'json'
+require 'pry'
+require 'csv'
+
+# this is an empty array where we will store all the hackernews
+hackernews = []
+
+# Page 1 to 3
+(1..3).each { |page|
+    # this is how we request the page we're going to scrape
+    page = HTTParty.get("https://news.ycombinator.com/news?p=#{page}")
+
+    # this is where we transform our http response into a nokogiri object
+    parse_page = Nokogiri::HTML(page)
+
+    # this is where we parse the data
+    parse_page.css('.title').css('.storylink').map do |a|
+        post_name = a.text
+        hackernews.push(post_name)
+    end
+}
+
+# this will push your array into a CSV file
+CSV.open('hackernews.csv', 'w') do |csv|
+    csv << hackernews
+end
+
+# pry.start(binding)
